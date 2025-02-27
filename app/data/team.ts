a91e6c6097ecdcd83411ab00da71c586c6f561ff
Kanishk Valechha <kanishkvalecha09@gmail.com>
2025-02-27 13:33:31 +0530

    Vedic marks

--- app/data/team.ts
+++ app/data/team.ts
@@ -4,15 +4,15 @@ import Kanishk from "../../public/Kanishk.jpg";
 export const teamMembers: TeamMember[] = [
   {
     id: 1,
+    name: "Vedic Varma",
+    role: "Full Stack Developer",
+    image:
+    "https://media.licdn.com/dms/image/v2/D4D03AQHs0nNvkfCeTA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1695459876909?e=1744243200&v=beta&t=5r1Uzy5gblDHqkA3imTBT8LWTRGoTWRdzRcRtMuCXFU",
+  },  
+  {
+    id: 2,
     name: "Kanishk Valechha",
     role: "Full Stack Developer",
     image: Kanishk.src,
   },
-  {
-    id: 2,
-    name: "Vedic Varma",
-    role: "Full Stack Developer",
-    image:
-      "https://media.licdn.com/dms/image/v2/D4D03AQHs0nNvkfCeTA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1695459876909?e=1744243200&v=beta&t=5r1Uzy5gblDHqkA3imTBT8LWTRGoTWRdzRcRtMuCXFU",
-  },  
 ];
